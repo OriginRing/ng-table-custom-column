6dd78d13e625f01430ae08a3c7b0003eed0d3408
originRing <yongyue_geng@163.com>
2023-03-20 15:20:09 +0800

    build: modify name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-custom-row'),
+      dir: require('path').join(__dirname, './coverage/ng-table-custom-column'),
       subdir: '.',
       reporters: [
         { type: 'html' },
